e9cc073a38999cf0693c7c6ad97ec30dde131cca
kevin <kestupinans@ucentral.edu.co>
2026-09-04 15:01:59 -0500

    DEVELOP COMMIT

--- src/main/java/com/ucentral/desarrollos/backendparkspotter/BackendParkSpotterApplication.java
+++ src/main/java/com/ucentral/desarrollos/backendparkspotter/BackendParkSpotterApplication.java
@@ -8,5 +8,6 @@ public class BackendParkSpotterApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendParkSpotterApplication.class, args);
         System.out.println("Backend Park Spotter Application is running...");
+        System.out.println("Logs");
     }
 }
