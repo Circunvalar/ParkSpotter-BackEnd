7ac00034f7556448f4f3e6f3f8c2527dc8137c7c
kevin <kestupinans@ucentral.edu.co>
2026-09-04 14:57:13 -0500

    MASTER COMMIT - Inicio de Flujo de trabajo GitFlow.

---
+++ src/main/java/com/ucentral/desarrollos/backendparkspotter/BackendParkSpotterApplication.java
@@ -0,0 +1,12 @@
+package com.ucentral.desarrollos.backendparkspotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendParkSpotterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendParkSpotterApplication.class, args);
+        System.out.println("Backend Park Spotter Application is running...");
+    }
+}
